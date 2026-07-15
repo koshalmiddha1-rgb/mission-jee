2339c92c81384d404c1007020d62df50db71913b
koshalmiddha1-rgb <koshalmiddha1@gmail.com>
2026-07-15 12:45:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let xp = Number(localStorage.getItem("xp")) || 0;
 let coins = Number(localStorage.getItem("coins")) || 0;
 let level = Number(localStorage.getItem("level")) || 1;
-
+let streak = Number(localStorage.getItem("streak")) || 0;
 const xpText = document.getElementById("xp");
 const coinsText = document.getElementById("coins");
 const levelText = document.getElementById("level");
 const progressBar = document.querySelector(".progress-bar");
-
+const missionButtons = document.querySelectorAll(".complete-btn");
 function updateUI() {
   xpText.textContent = xp;
   coinsText.textContent = coins;
   levelText.textContent = level;
-
+missionButtons.forEach(btn => {
+    if (btn.dataset.done === "true") {
+        btn.innerText = "Completed ✅";
+        btn.disabled = true;
+    }
+});
   progressBar.style.width = (xp % 100) + "%";
 
   localStorage.setItem("xp", xp);
